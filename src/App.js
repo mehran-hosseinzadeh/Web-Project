e68da6b84596cab6f1c2b7c1012ac21c09aa73d5
mohamadreza jafarzade <mrezajafarzade98@gmail.com>
2019-02-01 23:25:27 +0330

    add routing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,82 @@
 import React, {Component} from 'react';
 import './App.css';
-import PageHeader from "./components/home_page/PageHeader";
-import NewsPage from "./components/news/NewsPage";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import LeagueTab from "./components/league/LeagueTab";
 import HomePage from "./components/home_page/HomePage";
+import LeaguePage from "./components/league/LeaguePage";
+import TeamPage from "./components/team/TeamPage";
+import NewsPage from "./components/news/NewsPage";
 import GamePage from "./components/game/GamePage";
 import PlayerPage from "./components/player/PlayerPage";
-import TeamPage from "./components/team/TeamPage";
-import LeaguePage from "./components/league/LeaguePage";
+import PageHeader from "./components/home_page/PageHeader";
 import {Grid} from "semantic-ui-react";
+
 
 class App extends Component {
     render() {
         return (
-            [
-                <PageHeader className='row'/>,
-                <Grid>
-                    <Grid.Column width={16}>
-                        <HomePage/>
-                        <TeamPage/>
-                        <LeaguePage/>
-                        <NewsPage/>
-                        <GamePage/>
-                        <PlayerPage/>
-                    </Grid.Column>
-                </Grid>]
+            <Router>
+                <div>
+                    <PageHeader className='row'/>,
+                    <Grid>
+                        <Grid.Column width={16}>
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/league" component={League}/>
+                            <Route path="/team" component={Team}/>
+                            <Route path="/news" component={News}/>
+                            <Route path="/game" component={Game}/>
+                            <Route path="/player" component={Player}/>
+                        </Grid.Column>
+                    </Grid>
+
+                </div>
+            </Router>
         )
     }
+
 }
+
+const Home = () => <HomePage/>;
+const League = () => <LeaguePage/>;
+const Team = () => <TeamPage/>;
+const News = () => <NewsPage/>;
+const Game = () => <GamePage/>;
+const Player = () => <PlayerPage/>;
+// const About = () => <h2>About</h2>;
+// const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
+// const Topics = ({ match }) => (
+//     <div>
+//         <h2>Topics</h2>
+//
+//         <ul>
+//             <li>
+//                 <Link to={`${match.url}/components`}>Components</Link>
+//             </li>
+//             <li>
+//                 <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+//             </li>
+//         </ul>
+//
+//         <Route path={`${match.path}/:id`} component={Topic} />
+//         <Route
+//             exact
+//             path={match.path}
+//             render={() => <h3>Please select a topic.</h3>}
+//         />
+//     </div>
+// );
+// const Header = () => (
+//     <ul>
+//         <li>
+//             <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//             <Link to="/about">About</Link>
+//         </li>
+//         <li>
+//             <Link to="/topics">Topics</Link>
+//         </li>
+//     </ul>
+// );
 
 export default App;
--- src/components/home_page/PageHeader.js
+++ src/components/home_page/PageHeader.js
@@ -1,55 +1,57 @@
 import React, {Component} from 'react';
 import "./PageHeader.css"
-import {Input, Menu } from 'semantic-ui-react'
+import {Input, Menu} from 'semantic-ui-react'
+import {Link} from "react-router-dom";
 
 const colorsB = ['blue', 'violet', 'purple', 'pink', 'brown'];
 
 export default class PageHeader extends Component {
 
-    static defaultProps ={
+    static defaultProps = {
         menuOptions: [
-            {name: "خانه", color: "blue"},
-            {name: "فوتبال", color: "violet"},
-            {name: "بسکتبال", color: "purple"},
-            {name: "لیگ ها", color: "pink"},
-            {name: "سایر", color: "brown"},
+            {name: "خانه", color: "blue", url: "/"},
+            {name: "تیم", color: "violet", url: "/team"},
+            {name: "خبر", color: "purple", url: "/news"},
+            {name: "لیگ", color: "pink", url: "/league"},
+            {name: "بازی", color: "brown", url: "/game"},
+            {name: "بازیکن", color: "red", url: "/player"},
         ]
     };
 
-    state = { active: colorsB[0] };
+    state = {active: colorsB[0]};
 
-    handleItemClick = (e, { name }) => this.setState({ active: name });
+    handleItemClick = (e, {name}) => this.setState({active: name});
 
     render() {
         return (
             <div style={{direction: "rtl"}}>
                 <div className="pageHeader">
-                    <img className="pageLogo" src = {"pageLogo.png"} alt = "page logo"/>
+                    <img className="pageLogo" src={"pageLogo.png"} alt="page logo"/>
                 </div>
                 <Menu inverted style={{marginTop: "0"}}>
-                    {this.props.menuOptions.map(({name, color}, index) => (
+                    {this.props.menuOptions.map(({name, color, url}, index) => (
                         <Menu.Item
                             key={index}
-                            name={name}
                             active={this.state.active === name}
                             color={color}
-                            onClick={this.handleItemClick}
-                        />
-                    ))}
-                    <Menu.Menu>
+                            onClick={this.handleItemClick}>
+                            <Link to={url}>{name}</Link>
+                        </Menu.Item>
+                        ))}
+                        <Menu.Menu>
                         <Menu.Item
-                            name='خروج'
-                            active={this.state.active === 'logout'}
-                            color = "blue"
-                            onClick={this.handleItemClick}
+                        name='خروج'
+                        active={this.state.active === 'logout'}
+                        color = "blue"
+                        onClick={this.handleItemClick}
                         />
                         <Menu.Item style={{minWidth: "400px"}}>
-                            <Input icon='search' placeholder='...جست و جو'  />
+                        <Input icon='search' placeholder='...جست و جو'  />
                         </Menu.Item>
-                    </Menu.Menu>
+                        </Menu.Menu>
 
-                </Menu>
-            </div>
-        );
-    }
-}
+                        </Menu>
+                        </div>
+                        );
+                    }
+                    }
